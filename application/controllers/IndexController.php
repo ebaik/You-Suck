6360e85bb0a1c8532d65bd4a1229e38a5614dbbf
Liang Huang <liangdev@yahoo.com>
2011-12-19 07:38:09 +0000

    add company name to each post

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -20,45 +20,15 @@ class IndexController extends BaseController
     public function indexAction()
     {
     	try{
-	    	$exe = Zend_Registry::get("exe");
-	        $em = $exe->getMetaDataEntityManager();
-	        $query = $em->createQuery('select p.text, u.firstname, p.anonymous_flag from posts p, user u where u.id = p.user_id ORDER BY p.post_time desc ')->setMaxResults(10);
-	        $itr = $query->iterate();
-	        $i = 0;
-                $posts = array();
-	        foreach ($itr as $res) {
-                    $posts[$i]['post_text'] =  $res[$i]['text'];
-                    $fullname = array();
-                    if(!empty($res[$i]['anonymous_flag'])) 
-                    {
-                        $fullname = 'Anonymous';
-                    } else if(!empty($res[$i]['firstname']))
-                    {
-                        $fullname = $res[$i]['firstname'];
-                    }
-                    $posts[$i]['name'] =  $fullname;
-	            $i++;
-	        }
-	       
-	        $this->view->posts = $posts;
+                $ps = new PostService();
+                $posts = $ps->getMorePost();
+                $this->view->posts = $posts;
 	       
     	}
     	catch (Exception $ex) {
             error_log("Index exception:" . $ex->getMessage());
             return false;
         }
-    	
-        /*
-        var_dump($itr);
-        exit;
-        $posts = array();
-        foreach ($itr as $user) {
-            $posts[] =  $user[0];
-        }
-        return $posts;
-        */
     }
-
-
 }
 
