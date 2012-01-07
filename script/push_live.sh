fef1534d5908c69241236ae072045161a3589123
jianqing.ebay <jianqing.ebay@gmail.com>
2012-01-06 16:36:21 -0800

    tweak pushing live script

--- script/push_live.sh
+++ script/push_live.sh
@@ -5,5 +5,5 @@ cp -R -v ../* /var/www/yousuckapp
 release=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
 
 echo "==============================="
-echo "You pushed $release to live ！! "
+echo "You pushed $release to live !! "
 echo "==============================="
