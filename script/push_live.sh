2bf7e8b3a4176cd26939281e827e141b5d3ac5c3
jianqing.ebay <jianqing.ebay@gmail.com>
2012-01-06 16:35:59 -0800

    add pushing live script

---
+++ script/push_live.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cp -R -v ../* /var/www/yousuckapp
+
+release=`git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+
+echo "==============================="
+echo "You pushed $release to live ！! "
+echo "==============================="
